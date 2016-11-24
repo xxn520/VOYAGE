229724c57bc69002c24847a7d6434c242896a18a
m2mbob <2844870322@qq.com>
2016-11-24 10:42:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+## VOYAGE
+
+一个简单的 psd to html，使用 gulp 实现简单的工程化任务。
+
+### 目录说明
+
+- css
+ - main.css 主 css 文件
+ - vendor：css 库
+- dist：构建目标目录
+- fonts：字体目录
+- img：图片目录
+- js：js 目录
+- node_modules：各种 gulp 插件
+- psd：存放了原始 psd
+- editorconfig：编辑器配置
+- .gitignore：git 忽略配置
+- gulpfile.js：gulp 配置
+- index.html：首页
+- package.json：node 相关配置
+
+### 运行
+
+```
+git clone https://github.com/xxn520/VOYAGE.git
+
+npm install
+
+gulp
+```
+
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const htmlmin = require('gulp-htmlmin')
+const del = require('del')
+const minifycss = require('gulp-minify-css')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
+const staticPaths = ['img/*', 'fonts/*', 'js/vendor/*', 'css/vendor/*']
+
+// 用 htmlmin 模拟模板引擎，去掉空格，解决 inline-block 元素间距。
+gulp.task('minify', () =>
+	gulp.src('*.html')
+	    .pipe(htmlmin({collapseWhitespace: true}))
+	    .pipe(gulp.dest('dist'))
+	    .pipe(browserSync.stream())
+)
+
+// css 前缀、重命名、压缩。
+gulp.task('css', () =>
+	gulp.src('css/*.css')
+		.pipe(autoprefixer({
+            browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+            cascade: false
+        }))
+        .pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist/assets/css'))
+		.pipe(browserSync.stream())
+)
+
+// js 重命名、压缩。
+gulp.task('js', () => {
+	gulp.src('js/*.js')
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/assets/js'))
+		.pipe(browserSync.stream())
+})
+
+// 静态资源的拷贝
+gulp.task('static', () => {
+	staticPaths.forEach((path) => {
+		const dest = path.replace(/\/\*/, '')
+		gulp.src(path)
+			.pipe(gulp.dest(`dist/assets/${dest}`))
+			.pipe(browserSync.stream())
+	})
+})
+
+// 开启 browserSync 服务，并监听文件变化
+gulp.task('serve', () => {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    })
+
+    gulp.watch("*.html", ['minify'])
+    gulp.watch("js/*.js", ['js'])
+  	gulp.watch("css/*.css", ['css'])
+  	gulp.watch(staticPaths, ['static'])
+})
+
+gulp.task('default', ['static', 'css', 'js', 'minify', 'serve'])
